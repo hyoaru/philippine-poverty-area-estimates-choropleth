24da811e0b244a4ef7498dae00e23ddaa82ce667
hyoaru <jjcabreraaaa@gmail.com>
2023-04-14 15:58:33 +0800

    [add] page configurations

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import streamlit as st
 from app.data import get_dataframe, get_region_name_by_region_code, get_year_by_column_name, get_plot_by_province, get_plot_by_region
 from app.config import hide_streamlit_style
 
+st.set_page_config(page_title="PH Poverty Area Estimates", page_icon = ":round_pushpin:")
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 df = get_dataframe()
 region_name_by_region_code = get_region_name_by_region_code()
 region_names = [name for name in region_name_by_region_code.keys()]
 year_by_column_name = get_year_by_column_name()
 years = [year for year in year_by_column_name.keys()]
-
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 with st.container():
     st.title('Philippine Poverty Area Estimates Choropleth Visualization')
